8655510bff683bce08be4f7db7d64dd041021389
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-03-26 16:34:03 +0900

    スペルミス

--- migrations/4_deploy_METFaucet.js
+++ migrations/4_deploy_METFaucet.js
@@ -7,6 +7,6 @@ module.exports = function (deployer) {
     // 最初にMETokenをデプロイする。
     deployer.deploy(METoken, {from: owner}).then(function() {
         // METFaucetをデプロイする。
-        deployer.deploy(METFaucet, MEtoken.address, owner);
+        deployer.deploy(METFaucet, METoken.address, owner);
     });
 }
