c7e20f5ddbd3c849095c072f7459308f1cfea3e0
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-03-26 16:26:01 +0900

    マイグレーションファイルを追加する。

---
+++ migrations/4_deploy_METFaucet.js
@@ -0,0 +1,12 @@
+var METoken = artifacts.require("METoken");
+var METFaucet = artifacts.require("METFaucet");
+// アカウント情報を読み取る
+var owner = web3.eth.accounts[0];
+
+module.exports = function (deployer) {
+    // 最初にMETokenをデプロイする。
+    deployer.deploy(METoken, {from: owner}).then(function() {
+        // METFaucetをデプロイする。
+        deployer.deploy(METFaucet, MEtoken.address, owner);
+    })
+}
